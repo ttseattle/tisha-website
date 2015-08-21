c5b941e84b09ae044f4660cd5c927f5a380d45c5
Tisha <ttseattle@yahoo.com>
2015-08-21 10:38:43 -0700

    Added lots more content

--- js/index.js
+++ js/index.js
@@ -1,8 +1,27 @@
-$(document).ready(function() {
-	$('#hook, #design p, #write p, #create p, #inspire p').hide().each(function(i) {
-	  $(this).delay(i*500).fadeIn(800);
+$(window).load(function() {
+	$('#design p, #write p, #create p, #inspire').hide().each(function(i) {
+	  $(this).delay((i + 5) *500).fadeIn(800);
 	});
-	$('#inspire p').hover(function() {
-		$('#inspire img').toggle();
+	$('#inspire p').mouseenter(function() {
+		$('#bulb').attr("src", "img/bulb-light.png")
 	})
+	$('#inspire p').mouseleave(function() {
+		$('#bulb').attr("src", "img/bulb-dark.png")
+	})
+	/*
+	$('#design p').mouseenter(function() {
+		$('#desktop').attr('src', 'img/stencil.jpg')
+	})
+	$('#write p').mouseenter(function() {
+		$('#desktop').attr('src', 'img/typewriter.jpg')
+	})
+	$('#create p').mouseenter(function() {
+		$('#desktop').attr('src', 'img/bw.jpg')
+	})
+	$('#inspire p').mouseenter(function() {
+		$('#desktop').attr('src', 'img/sparkler.jpg')
+	})
+	$('#design p, #write p, #create p, #inspire p').mouseleave(function() {
+		$('#desktop').attr('src', 'img/desk4.jpg')
+	})*/
 })
